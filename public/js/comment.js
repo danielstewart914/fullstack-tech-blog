52bdcb2dbc9cb00228c64ffcb3b50e2426f72d59
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-09-10 09:40:50 -0700

    Consolidated newPost.js into post.js and updated tooltips

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,34 +1,42 @@
 const newCommentEl = document.querySelector( '#new-comment' );
 const postCommentButton = document.querySelector( '#post-comment' );
-const postEl = document.querySelector( '#post' );
-let emptyCommentToolTip;
+const postIdEl = document.querySelector( '#post' );
+
+const emptyCommentToolTip = new bootstrap.Tooltip( newCommentEl );
+let showingCommentToolTip = false;
 
 const postComment = async () => {
     const comment = newCommentEl.value.trim();
 
+    // if comment input is blank
     if ( !comment ) {
-        emptyCommentToolTip.show();
+        // if not showing comment tooltip
+        if ( !showingCommentToolTip ) {
+            // display tooltip
+            emptyCommentToolTip.show();
+            showingCommentToolTip = true;
+            // hide comment tooltip after 1.5 seconds
+            setTimeout( () => {
+                emptyCommentToolTip.hide();
+                showingCommentToolTip = false;
+        }, 1500 );
+        }
         return;
     }
 
-    const post_id = postEl.dataset.id;
+    // store post id from data attribute
+    const post_id = postIdEl.dataset.id;
 
+    // post comment
     const response = await fetch( '/api/comments', {
         method: 'POST',
         body: JSON.stringify( { comment, post_id } ),
         headers: { 'Content-Type': 'application/json' }
     } );
 
+    // reload page
     if ( response.ok ) window.location.reload();
     else alert( 'Cannot Post Comment!' );
 }
 
 postCommentButton.addEventListener( 'click', postComment );
-
-postCommentButton.addEventListener( 'focusout', () => {
-    emptyCommentToolTip.hide();
-} );
-
-document.addEventListener( 'DOMContentLoaded' , () => {
-    emptyCommentToolTip = new bootstrap.Tooltip( newCommentEl );
-} );
--- public/js/post.js
+++ public/js/post.js
@@ -1,73 +1,97 @@
+// new post submit button
+const submitPostButton = document.querySelector( '#submit-post' );
+
+// edit post buttons
 const editButton = document.querySelector( '#edit-post' );
 const saveButton = document.querySelector( '#save-edit' );
 const cancelButton = document.querySelector( '#cancel-edit' );
 const deleteButton = document.querySelector( '#delete-post' );
-const titleEl = document.querySelector( '#edit-title' );
-const contentEl = document.querySelector( '#edit-content' );
+
+// title and content
+const titleEl = document.querySelector( '#title' );
+const contentEl = document.querySelector( '#content' );
+const postEl = document.querySelector( '#post' );
+
+// views to show or hide in edit mode
 const editViews = document.querySelectorAll( '.edit' );
 
 // tool tips
-let emptyTitleToolTip;
-let emptyContentToolTip;
+const emptyTitleToolTip = new bootstrap.Tooltip( titleEl );
+const emptyContentToolTip = new bootstrap.Tooltip( contentEl );
+let showingToolTip = false;
+
+// API URL's for creating new post or updating existing post
+const apiURL = {
+    POST: '/api/posts',
+    PUT: `/api/posts/${ postEl.dataset.id }`
+}
 
 // toggle edit and display elements
 const editPost = ( event ) => {
     editViews.forEach( view => view.classList.toggle( 'd-none' ) );
 }
 
-// PUT changes to posts route
-const saveChanges = async ( event ) => {
+// check if element is blank
+const isBlank = ( input, toolTip ) => {
+
+    // if input is blank
+    if ( !input ) {
+        // if not showing tooltip
+        if ( !showingToolTip ) {
+            toolTip.show();
+            showingToolTip = true;
+            // hide tooltip after 1.5 seconds
+            setTimeout( () => {
+                toolTip.hide();
+                showingToolTip = false;
+            }, 1500 );
+        }
+        // return true for blank element
+        return true;
+    }
+    // return false for populated element
+    return false;
+}
+
+// save post
+const savePost = async ( event ) => {
+
     const title = titleEl.value.trim();
     const content = contentEl.value.trim();
 
-    // check if title is blank
-    if ( !title ) {
-        // display tool tip
-        emptyTitleToolTip.show();
-        return;
-    }
-
-    // check if content is empty
-    if ( !content ) {
-        // display tool tip
-        emptyContentToolTip.show();
-        return;
-    }
-
-    // update post
-    const response = await fetch( `/api/posts/${ postEl.dataset.id }`, { 
-        method: 'PUT',
+    // check if title or content is blank and display tooltip
+    if ( isBlank( title, emptyTitleToolTip ) ) return;
+    if ( isBlank( content, emptyContentToolTip ) ) return;
+ 
+    // send either a POST or PUT request - method is stored in a button data attribute
+    const response = await fetch( apiURL[ event.target.dataset.method ], {
+        method: event.target.dataset.method,
         body: JSON.stringify( { title, content } ),
         headers: { 'Content-Type': 'application/json' }
-     } );
+    } );
 
-    //  reload window
-     if ( response.ok ) window.location.reload();
-     else alert( 'Cannot Update Post!' );
+    if ( response.ok )  {
+        // either refresh page or send user to dashboard
+        if ( event.target.dataset.method === 'POST' ) window.location.replace( '/dashboard' );
+        if ( event.target.dataset.method === 'PUT' ) window.location.reload();
+    }
+    else alert( 'Cannot Save Post!' );
 }
 
+// delete post
 const deletePost = async ( event ) => {
     // delete post
     const response = await fetch( `/api/posts/${ postEl.dataset.id }`, {
         method: 'DELETE'
     } );
-    // reload window
+    // send user to their dashboard window
     if ( response.ok ) window.location.replace( '/dashboard' );
     else alert( 'Cannot Delete Post!' );
 }
 
-editButton.addEventListener( 'click', editPost );
-saveButton.addEventListener( 'click', saveChanges );
-cancelButton.addEventListener( 'click', () => window.location.reload() );
-deleteButton.addEventListener( 'click', deletePost );
-
-
-saveButton.addEventListener( 'focusout', () => {
-    emptyTitleToolTip.hide();
-    emptyContentToolTip.hide();
-} );
-
-document.addEventListener( 'DOMContentLoaded' , () => {
-    emptyTitleToolTip = new bootstrap.Tooltip( titleEl );
-    emptyContentToolTip = new bootstrap.Tooltip( contentEl );
-} );
+// optional event listeners for either newPost or post views
+if ( editButton ) editButton.addEventListener( 'click', editPost );
+if ( saveButton ) saveButton.addEventListener( 'click', savePost );
+if ( cancelButton ) cancelButton.addEventListener( 'click', () => window.location.reload() );
+if ( deleteButton ) deleteButton.addEventListener( 'click', deletePost );
+if ( submitPostButton ) submitPostButton.addEventListener( 'click', savePost );
